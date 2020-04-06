4b621844d11945dec677ba194c58ded7b7254bf6
kevroi <kevinroice@outlook.com>
2020-04-06 01:18:05 +0100

    Added User Authorization to articles

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -6,25 +7,34 @@ from . import models
 
 # Create your views here.
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
 	model = models.Article
 	template_name = 'article_list.html'
+	login_url = 'login'
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
 	model = models.Article
 	template_name = 'article_detail.html'
+	login_url = 'login'
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
 	model = models.Article
 	fields = ['title', 'body', ]
 	template_name = 'article_edit.html'
+	login_url = 'login'
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
 	model = models.Article
 	template_name = 'article_delete.html'
 	success_url = reverse_lazy('article_list')
+	login_url = 'login'
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView): #use this mixin to only allow loggedin users to create posts
 	model = models.Article
 	template_name = 'article_new.html'
-	fields = ['title', 'body', 'author', ]
+	fields = ['title', 'body', ] #removed author from fields and created below function for authorisation
+	login_url = 'login' #overrides default login page to get redirected to from accounts/login to users/login, where our login page is
+	
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
